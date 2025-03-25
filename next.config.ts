71fb372b489f068a09be51b6fe63bd03a456780d
AchilleasLeiv <a.leivadiotis@ng-sensors.com>
2025-03-25 22:05:06 +0100

    Fix?????

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: 'export',
   images: {
     unoptimized: true
-  },
-  // Disable trailing slashes in URLs
-  trailingSlash: false,
-  // SWC minify is now default in Next.js 15, no need to specify it
+  }
 }
 
 module.exports = nextConfig

d8d200a7a6becfac60de2c0befffb6175b70e3db
AchilleasLeiv <a.leivadiotis@ng-sensors.com>
2025-03-28 20:41:57 +0100

    Ànalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import HomeButton from "../components/ui/HomeButton";
-
+import { Analytics } from "@vercel/analytics/react"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,6 +31,7 @@ export default function RootLayout({
           {children}
         </div>
         <HomeButton />
+        <Analytics />
       </body>
     </html>
   );
